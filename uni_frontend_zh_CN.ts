162f659351b5eadc05c2cc2e1faeb85c7da452ae
Parasy Celena <mizuhasi_parsee@outlook.com>
2021-10-14 13:39:52 +0800

    Update zh translation

--- uni_frontend_zh_CN.ts
+++ uni_frontend_zh_CN.ts
@@ -6,33 +6,33 @@
     <message>
         <location filename="mainwindow.ui" line="32"/>
         <source>Uni-Frontend</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">通用前端</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="118"/>
         <source>Scale Ratio:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">输出比例：</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="131"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">添加文件</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="144"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">移除选中文件</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="157"/>
         <source>Run</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">开始执行</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="170"/>
         <location filename="mainwindow.ui" line="186"/>
         <source>This Context will be append to output file name.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">此文本将追加到输出文件名</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="173"/>
@@ -42,22 +42,22 @@
     <message>
         <location filename="mainwindow.ui" line="189"/>
         <source>Appending:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">追加后缀：</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="218"/>
         <source>Model:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">神经网络模型：</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="231"/>
         <source>Select Model</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">选择模型</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="254"/>
         <source>Clear</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">清空所有</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="272"/>
@@ -67,52 +67,52 @@
     <message>
         <location filename="mainwindow.ui" line="285"/>
         <source>Backend:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">后端：</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="308"/>
         <source>Select Backend</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">选择后端</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="321"/>
         <source>Enable tta</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">启用tta</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="334"/>
         <source>Clear Output</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">清除输出</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="124"/>
         <source>Add Files</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">添加文件</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="138"/>
         <source>ERROR</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">错误！</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="139"/>
         <source>Fail opening file: </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">打开文件失败：</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="141"/>
         <source>We will skip this file</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">将跳过此文件</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="208"/>
         <source>Select backend executable:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">选择后端可执行文件：</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="219"/>
         <source>Select pre-trained neural network model:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">选择已训练的神经网络模型：</translation>
     </message>
 </context>
 </TS>
